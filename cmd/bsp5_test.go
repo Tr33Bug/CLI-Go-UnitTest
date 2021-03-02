2410a84d9c70e6e87792989aeec9f904fe24d7b7
Tr33Bug <tr33@gmx.com>
2021-03-02 13:55:21 +0100

    add test for racecondition

---
+++ cmd/bsp5_test.go
@@ -0,0 +1,33 @@
+package cmd
+
+// Importieren des Testing-Paketes
+import (
+	"testing"
+)
+
+// Showcase for raceconditions in tests
+var superCount int
+
+func TestStartRace(t *testing.T) {
+	// Start subtest
+	t.Run("raceDown", func(t *testing.T) {
+		t.Parallel()
+		countDown(t)
+	})
+	// Start second subtest
+	t.Run("raceUp", func(t *testing.T) {
+		t.Parallel()
+		countUp(t)
+	})
+
+}
+
+func countUp(t *testing.T) {
+	superCount := 1
+	t.Log(superCount)
+}
+
+func countDown(t *testing.T) {
+	superCount := -1
+	t.Log(superCount)
+}
